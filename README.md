5044fcc6a07d05a858786b94f93fa180c38fe59d
Bo Xuan <tangboxuan1999@gmail.com>
2022-07-25 14:43:04 +0800

    Py2exe

---
+++ README.md
@@ -0,0 +1,27 @@
+# REpyexe
+
+## Description
+
+This script aims to decompile any Windows executable file compiled using Python into the original Python file.
+
+## Supported Files
+
+- [x] Python 2 using Py2exe
+- [x] Python 3 using Py2exe
+- [ ] Python 2 using PyInstaller
+- [ ] Python 3 using PyInstaller 
+
+## Usage
+
+```
+python decompile.py <exe_file>
+```
+The version of Python used must be the same minor version as the one used to compile the file. If a different one is chosen, the script will exit with a message telling you which version to use. Use pyenv or something similar to switch your Python version.
+
+## Background
+
+Most of the Python executable files out there are compiled using Py2exe or PyInstaller. While this script aims to decompile any exe file, it is currently limited to only those compiled using these 2 libraries.
+
+## Files
+
+- exe_files/main.exe: Hello World compiled using Py2exe in 3.7
---
+++ decompile.py
@@ -0,0 +1,49 @@
+# compatible with both Python 2 and Python 3
+
+import pefile 
+import sys, dis, marshal, re, os, subprocess, platform
+from uncompyle6.main import decompile
+
+def get_rsrc(pe, name):
+    for resource_type in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+        if str(resource_type.name) != name :
+            continue
+        for resource_id in resource_type.directory.entries: 
+            if not hasattr(resource_id, 'directory'):
+                continue
+            entry = resource_id.directory.entries[0]
+            rsrc = pe.get_data(entry.data.struct.OffsetToData, entry.data.struct.Size)
+            return rsrc 
+    return None
+
+def check_python_version(filename):
+    command = "strings " + fileName + " | grep \"^python[0-9][0-9].dll$\""
+    pythonVersion = platform.python_version()
+    dll = str(subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read())
+    exeVersion = '.'.join(dll.split('.')[0][-2:])
+    if not pythonVersion.startswith(exeVersion):
+        print("Python " + exeVersion + " required")
+        sys.exit()
+
+if __name__ == "__main__":
+    # fileName = sys.argv[1]
+    if platform.python_version().startswith("2"): 
+        fileName = "exe_files/khaki.exe"
+    else:
+        fileName = "exe_files/main.exe"
+    check_python_version(fileName)
+    pe = pefile.PE(fileName)
+    rsrc = get_rsrc(pe, "PYTHONSCRIPT")
+
+    if rsrc != None and rsrc[:4] == b"\x12\x34\x56\x78":
+        offset = rsrc[0x010:].find(b"\x00") 
+        if offset >= 0:
+            data = rsrc[0x10 + offset + 1:]
+            py2exeCode = marshal.loads(data)[-1]
+            # dis.dis(py2exeCode)
+            decompile(None, py2exeCode, sys.stdout)
+            print()
+        else:
+            print("Failed to find end of header")
+    else:
+        print("Failed to find PYTHONSCRIPT resource")
